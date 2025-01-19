7b8486a2e8a272d7fdbc9660c7d54ae39bd05dfd
NiniLovely1 <ninitaylor78@gmail.com>
2025-01-19 11:28:15 -0700

    improved the UX

--- src/index.js
+++ src/index.js
@@ -13,11 +13,14 @@ function generatePoem(event) {
 
   let specificsInput = document.querySelector("#user-specifics");
   let apiKey = "46a7o7054af22ba1t49c85122b081838";
-
   let context =
     "You are an expert Poet that loves to write beautiful and short love poems. Please generate a 4 line poem in basic HTML including a space between each line. Make sure to follow the user specifics. Do not inclde the words `HTML` before and after the poem.";
   let prompt = `User specifics: Generate a love poem about ${specificsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemFormElement = document.querySelector("#poem");
+  poemFormElement.classList.remove("hide");
+  poemFormElement.innerHTML = `<div class="blink"> Creating love poem about ${specificsInput.value}</div>`;
 
   console.log("Generating your poem");
   console.log(`Prompt: ${prompt}`);
